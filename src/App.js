add513a261adac15fb46ab16de8452343417bddf
Akshay singh <ww.akshay@gmail.com>
2024-06-02 18:22:26 +0530

    added testing elements

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import DashboardPage from "./pages/DashboardPage/DashboardPage.jsx";
 import TakeQuiz from "./components/TakeQuiz/TakeQuiz.jsx";
 import ThankYouPage from "./components/Thankyou/ThankYouPage.jsx";
 import PrivateRoute from "./components/PrivateRoute/PrivateRoute.jsx";
+import TestingPage from "./pages/HomePage/TestingPage.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/quiz/take/:uniqueLink" element={<TakeQuiz />} />
+        <Route path="/test/:uniqueLink" element={<TestingPage />} />
         <Route path="/auth" element={<AuthPage />} />
         <Route
           path="/"
